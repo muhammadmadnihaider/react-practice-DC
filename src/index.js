75f1201530779689b82e814b9096b1d5cb752710
Muhammad Madni <44685932+muhammadmadnihaider@users.noreply.github.com>
2020-08-08 12:57:10 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
 import "./index.css";
 
-function Avatar() {
+const testTweet = {
+  message: "Something about cats.",
+  gravatar: "xyz",
+  author: {
+    handle: "catperson",
+    name: "I AM A Cat Person",
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: "2016-07-30 21:24:37",
+};
+
+function Avatar({ hash }) {
   return (
     <img
-      src="https://www.gravatar.com/avatar/nothing"
+      src={`https://www.gravatar.com/avatar/nothing${hash}`}
       className="avatar"
       alt="avatar"
     />
   );
 }
-function Message() {
-  return <div className="message">This is less than 140 characters</div>;
+function Message({ text }) {
+  return <div className="message">{text}</div>;
 }
 
-function Author() {
+function Author({ author }) {
   return (
     <span className="author">
-      <span className="name">Your Name</span>
-      <span className="handle">@yourHandle</span>
+      <span className="name">{author.name}</span>
+      <span className="handle">@{author.handle}</span>
     </span>
   );
 }
-const Time = () => <span className="time">3h ago</span>;
+const Time = ({ time }) => (
+  <span className="time">{moment(time).fromNow()}</span>
+);
 const ReplyButton = () => <i className="fa fa-reply reply-button" />;
 const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
 const LikeButton = () => <i className="fa fa-heart like-button" />;
@@ -31,14 +46,15 @@ const MoreOptionsButton = () => (
   <i className="fa fa-ellipsis-h more-options-button" />
 );
 
-function Tweet() {
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <Avatar />
+      <Avatar hash={tweet.gravatar} />
       <div className="content">
-        <Author />
-        <Time />
-        <Message />
+        <Author author={tweet.author} />
+        <Time time={tweet.timestamp} />
+
+        <Message text={tweet.message} />
 
         <div className="buttons">
           <ReplyButton />
@@ -51,4 +67,4 @@ function Tweet() {
   );
 }
 
-ReactDOM.render(<Tweet />, document.querySelector("#root"));
+ReactDOM.render(<Tweet tweet={testTweet} />, document.querySelector("#root"));

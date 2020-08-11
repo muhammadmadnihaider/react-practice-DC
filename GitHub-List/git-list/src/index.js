7f281caa19766bb1a9858c93b7732776b74da88a
Muhammad Madni <44685932+muhammadmadnihaider@users.noreply.github.com>
2020-08-11 15:49:35 +0500

    adding

---
+++ GitHub-List/git-list/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const FileList = () => {
+  return <div>File List</div>;
+};
+
+ReactDOM.render(<FileList />, document.querySelector("#root"));

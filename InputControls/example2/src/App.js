68718bfaade2f20d61e37f4c8909aaf8f65035f8
Muhammad Madni <44685932+muhammadmadnihaider@users.noreply.github.com>
2020-08-23 21:43:37 +0500

    adding

---
+++ InputControls/example2/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <label>
+        firstName:
+        <input type="text" />
+      </label>
+      <label>
+        firstName:
+        <input type="text" />
+      </label>
+      <button>Submit</button>
+    </div>
+  );
+};
+export default App;

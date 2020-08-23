e20b4349f794faa5d785c0f6141c0c1aaef33880
Muhammad Madni <44685932+muhammadmadnihaider@users.noreply.github.com>
2020-08-23 23:24:06 +0500

    adding

--- InputControls/example2/src/App.js
+++ InputControls/example2/src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const [fName, setFName] = useState();
+  const [lName, setLName] = useState();
+  const firstName = useRef("");
+  const lastName = useRef("");
+  const handlerClick = () => {
+    // setDisplay(firstName || lastName);
+    setFName(firstName.current.value);
+    setLName(lastName.current.value);
+  };
   return (
     <div>
       <label>
         firstName:
-        <input type="text" />
+        <input ref={firstName} type="text" />
       </label>
       <label>
         firstName:
-        <input type="text" />
+        <input ref={lastName} type="text" />
       </label>
-      <button>Submit</button>
+      {fName || lName ? (
+        <h1>
+          {fName}
+          {lName}
+        </h1>
+      ) : null}
+      <button onClick={handlerClick}>Submit</button>
     </div>
   );
 };

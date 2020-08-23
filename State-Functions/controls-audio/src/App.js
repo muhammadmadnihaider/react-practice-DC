37979a15922807f849f4b8704cf3f33ba2ee6c51
Muhammad Madni <44685932+muhammadmadnihaider@users.noreply.github.com>
2020-08-23 13:05:39 +0500

    object

--- State-Functions/controls-audio/src/App.js
+++ State-Functions/controls-audio/src/App.js
@@ -3,54 +3,104 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  const [volume, setVolume] = useState(99);
-  const [treble, setTreble] = useState(99);
-  const [mid, setMid] = useState(99);
-  const [bass, setBass] = useState(99);
+  // const [volume, setVolume] = useState(99);
+  // const [treble, setTreble] = useState(99);
+  // const [mid, setMid] = useState(99);
+  // const [bass, setBass] = useState(99);
+
+  const [audio, setAudio] = useState({
+    volume: 99,
+    mid: 99,
+    bass: 99,
+    treble: 99,
+  });
 
   const incVolume = () =>
-    setVolume((volume) => {
-      if (volume == 0 || volume == 100) return volume;
-      return volume + 1;
+    setAudio((audio) => {
+      if (audio.volume === 100) return audio;
+      return { ...audio, volume: audio.volume + 1 };
     });
   const decVolume = () =>
-    setVolume((volume) => {
-      if (volume == 0 || volume == 100) return volume;
-      return volume - 1;
+    setAudio((audio) => {
+      if (audio.volume === 0) return audio;
+      return { ...audio, volume: audio.volume - 1 };
     });
 
   const incTreble = () =>
-    setTreble((treble) => {
-      if (treble == 0 || treble == 100) return treble;
-      return treble + 1;
+    setAudio((audio) => {
+      if (audio.treble === 100) return audio;
+      return { ...audio, treble: audio.treble + 1 };
     });
   const decTreble = () =>
-    setTreble((treble) => {
-      if (treble == 0 || treble == 100) return treble;
-      return treble - 1;
+    setAudio((audio) => {
+      if (audio.treble === 0) return audio;
+      return { ...audio, treble: audio.treble - 1 };
     });
 
   const incMid = () =>
-    setMid((mid) => {
-      if (mid == 0 || mid == 100) return mid;
-      return mid + 1;
+    setAudio((audio) => {
+      if (audio.mid === 100) return audio;
+      return { ...audio, mid: audio.mid + 1 };
     });
   const decMid = () =>
-    setMid((mid) => {
-      if (mid == 0 || mid == 100) return mid;
-      return mid - 1;
+    setAudio((audio) => {
+      if (audio.mid === 0) return audio;
+      return { ...audio, mid: audio.mid - 1 };
     });
 
   const incBass = () =>
-    setBass((bass) => {
-      if (bass == 0 || bass == 100) return bass;
-      return bass + 1;
+    setAudio((audio) => {
+      if (audio.bass === 100) return audio;
+      return { ...audio, bass: audio.bass + 1 };
     });
   const decBass = () =>
-    setBass((bass) => {
-      if (bass == 0 || bass == 100) return bass;
-      return bass - 1;
+    setAudio((audio) => {
+      if (audio.bass === 0) return audio;
+      return { ...audio, bass: audio.bass - 1 };
     });
+  // const incVolume = () =>
+  //   setVolume((volume) => {
+  //     if (volume == 0 || volume == 100) return volume;
+  //     return volume + 1;
+  //   });
+  // const decVolume = () =>
+  //   setVolume((volume) => {
+  //     if (volume == 0 || volume == 100) return volume;
+  //     return volume - 1;
+  //   });
+
+  // const incTreble = () =>
+  //   setTreble((treble) => {
+  //     if (treble == 0 || treble == 100) return treble;
+  //     return treble + 1;
+  //   });
+  // const decTreble = () =>
+  //   setTreble((treble) => {
+  //     if (treble == 0 || treble == 100) return treble;
+  //     return treble - 1;
+  //   });
+
+  // const incMid = () =>
+  //   setMid((mid) => {
+  //     if (mid == 0 || mid == 100) return mid;
+  //     return mid + 1;
+  //   });
+  // const decMid = () =>
+  //   setMid((mid) => {
+  //     if (mid == 0 || mid == 100) return mid;
+  //     return mid - 1;
+  //   });
+
+  // const incBass = () =>
+  //   setBass((bass) => {
+  //     if (bass == 0 || bass == 100) return bass;
+  //     return bass + 1;
+  //   });
+  // const decBass = () =>
+  //   setBass((bass) => {
+  //     if (bass == 0 || bass == 100) return bass;
+  //     return bass - 1;
+  //   });
 
   // const [audio,setAudio] = useState({volume:})
 
@@ -62,7 +112,7 @@ const App = () => {
             -
           </button>
           <span className="text-center col-5 font-weight-bold">
-            {volume}
+            {audio.volume}
             <label className="col text-secondary">VOLUME</label>
           </span>
 
@@ -75,7 +125,7 @@ const App = () => {
             -
           </button>
           <span className="text-center col-5 font-weight-bold">
-            {treble}
+            {audio.treble}
             <label className="col text-secondary">TREBLE</label>
           </span>
           <button className="col-2" onClick={incTreble}>
@@ -87,7 +137,7 @@ const App = () => {
             -
           </button>
           <span className="text-center col-5 font-weight-bold">
-            {mid}
+            {audio.mid}
             <label className="col text-secondary">MID</label>
           </span>
           <button className="col-2" onClick={incMid}>
@@ -100,7 +150,7 @@ const App = () => {
             -
           </button>
           <span className="text-center col-5 font-weight-bold">
-            {bass}
+            {audio.bass}
             <label className="col text-secondary">BASS</label>
           </span>
           <button className="col-2" onClick={incBass}>
